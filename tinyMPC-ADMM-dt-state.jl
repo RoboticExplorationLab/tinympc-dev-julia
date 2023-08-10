005b8b6e0c62adb70fe7f8cf68192da984d84efb
Sam Schoedel <seschoedel@gmail.com>
2023-08-10 12:03:30 -0400

    inequality constraints

--- tinyMPC-ADMM-dt-state.jl
+++ tinyMPC-ADMM-dt-state.jl
@@ -1,3 +1,5 @@
+using GeometryBasics: HyperSphere
+
 #ADMM Functions
 function backward_pass_grad!(q, r, p, d, params)
     #This is just the linear/gradient term from the backward pass (no cost-to-go Hessian or K calculations)
@@ -6,6 +8,7 @@ function backward_pass_grad!(q, r, p, d, params)
         # display("short")
         d[k] .= cache.Quu_inv*(cache.B̃'*p[k+1] + r[k])
         p[k] .= q[k] + cache.AmBKt*p[k+1] - cache.Kinf'*r[k] + cache.coeff_d2p*d[k]
+        # p[k] .= q[k] + cache.Ã'*cache.Pinf*cache.B̃*d[k] - cache.Ã'*p[k+1]
         # display(q[k])
         # display(p[k])
     end
@@ -19,20 +22,47 @@ function forward_pass!(d, x, u, params)
     end
 end
 
+# Updates x and u (decision variables)
 function update_primal!(q, r, p, d, x, u, params)
     backward_pass_grad!(q, r, p, d, params)
     forward_pass!(d, x, u, params)
 end
 
-function update_slack!(x, v, g, u, z, y, params)
+function project_hyperplane(k, vis, x, A, q)
+    a = A[1:2]
+    x_xy = x[1:2]
+    if a'*x_xy - a'*q <= 0
+        return x
+    else
+        x_xy_new = [a[2]^2 -a[1]*a[2]; -a[1]*a[2] a[1]^2]/(a[1]^2+a[2]^2) * (x_xy-q) + q
+
+        if k == 1
+            # display(a'*x_xy_new - a'*q)
+
+            # sleep(.1)
+            # Visualize solution
+            delete!(vis["z_location"])
+            delete!(vis["z_proj_location"])
+            setobject!(vis["z_location"], HyperSphere(Point{3, Float64}(x_xy[1], x_xy[2], .5), .02),
+                LineBasicMaterial(color=Colors.RGBA(0,0,1)))
+            setobject!(vis["z_proj_location"], HyperSphere(Point{3, Float64}(x_xy_new[1], x_xy_new[2], .5), .02),
+                LineBasicMaterial(color=Colors.RGBA(1,0,0)))
+        end
+
+        return [x_xy_new; x[3:end]]
+    end
+end
+
+function update_slack!(vis, x, v, g, u, z, y, params)
     #This function clamps the controls to be within the bounds
     for k = 1:(params.N-1)
         z[k] .= min.(params.umax, max.(params.umin, u[k] + y[k])) # TODO: convert u[k] + y[k] to params.A*u[k] + params.b + y[k] or combine into one slack and dual update
-        v[k] .= min.(params.xmax[k], max.(params.xmin[k], params.A[k]*x[k] + g[k]))
     end
-    # for k = 1:params.N
-    #     v[k] .= min.(params.xmax[k], max.(params.xmin[k], params.A[k]*x[k] + g[k]))
-    # end
+    for k = 1:params.N
+        # v[k] .= min.(params.xmax[k], max.(params.xmin[k], params.A[k]*x[k] + g[k] ))
+        v[k] .= project_hyperplane(k, vis, x[k]-g[k], params.A[k], params.xmax[k])
+    end
+    # display(xmax[1][1])
 end
 
 function update_dual!(x, v, g, u, z, y, params)
@@ -40,24 +70,25 @@ function update_dual!(x, v, g, u, z, y, params)
     #Note that we're using the "scaled form" where y = λ/ρ
     for k = 1:(params.N-1)
         y[k] .= y[k] + u[k] - z[k]
-        g[k] .= g[k] + params.A[k]*x[k] - v[k]
     end
-    # for k = 1:params.N
-    #     g[k] .= g[k] + x[k] - v[k]
-    # end
+    for k = 1:params.N
+        g[k] .= g[k] + x[k] - v[k]
+    end
 end
 
 function update_linear_cost!(v, g, z, y, p, q, r, ρ, params)
     #This function updates the linear term in the control cost to handle the changing cost term from ADMM
     for k = 1:(params.N-1)
         r[k] .= -ρ*(z[k]-y[k]) - params.R*params.Uref[k] # original R
-        q[k] .= -ρ*(v[k]-g[k]) - params.Q*params.Xref[k]
+        q[k] .= -ρ*g[k]*10 - params.Q*params.Xref[k]
+        # q[k] .= -ρ*(v[k]-g[k]) - params.Q*params.Xref[k]
+        # q[k] .= -(v[k] - params.Q*params.Xref[k])
     end
-    # for k = 1:params.N
-    #     q[k] .= -ρ*(v[k]-g[k]) - params.Q*params.Xref[k]
-    #     # q[k] .= -params.Q*params.Xref[k]
-    # end
-    p[params.N] .= -params.cache.Pinf*params.Xref[params.N]
+    display(g[params.N])
+    # p[params.N] .= -v[params.N] + ρ*g[params.N] - params.cache.Pinf*params.Xref[params.N]
+    # p[params.N] .= -ρ*(v[params.N]-g[params.N]) + params.cache.Pinf*params.Xref[params.N]
+    # p[params.N] .= -params.cache.Pinf*params.Xref[params.N]
+    p[params.N] .= -ρ*g[params.N]*10 - params.cache.Pinf*params.Xref[params.N]
 end
 
 #Main algorithm loop
@@ -70,7 +101,7 @@ function solve_admm!(vis, params, q, r, p, d, x,v,vnew,g, u,z,znew,y; ρ=1.0, ab
     iter = 1
 
     forward_pass!(d, x, u, params)
-    update_slack!(x, v, g, u, z, y, params)
+    update_slack!(vis, x, v, g, u, z, y, params)
     update_dual!(x, v, g, u, z, y, params)
     update_linear_cost!(v, g, z, y, p, q, r, ρ, params)
     for k = 1:max_iter
@@ -79,7 +110,7 @@ function solve_admm!(vis, params, q, r, p, d, x,v,vnew,g, u,z,znew,y; ρ=1.0, ab
         update_primal!(q, r, p, d, x, u, params)
 
         #Project z into feasible domain
-        update_slack!(x, vnew, g, u, znew, y, params)
+        update_slack!(vis, x, vnew, g, u, znew, y, params)
 
         #Dual ascent
         update_dual!(x, vnew, g, u, znew, y, params)
@@ -88,36 +119,22 @@ function solve_admm!(vis, params, q, r, p, d, x,v,vnew,g, u,z,znew,y; ρ=1.0, ab
         
         primal_residual = maximum(abs.(hcat(u...) - hcat(znew...)))
         dual_residual_input = maximum(abs.(ρ*(hcat(znew...) - hcat(z...))))
-        dual_residual_state = maximum(abs.(ρ*(hcat(vnew...) - hcat(v...))))
+        # dual_residual_state = maximum(abs.(ρ*(hcat(vnew...) - hcat(v...))))
 
-        # display("iter")
-        # display(iter)
-        # display(z)
-        # display(znew)
-        # display(primal_residual)
-        # display(dual_residual_input)
-        # display(dual_residual_state)
         
         z = deepcopy(znew)
         v = deepcopy(vnew)
 
         
         if (primal_residual < abs_tol && 
-            dual_residual_input < abs_tol &&
-            dual_residual_state < abs_tol)
+            dual_residual_input < abs_tol) # &&
+            # dual_residual_state < abs_tol)
             status = 1
             break
         end
 
         iter += 1
 
-        # sleep(.5)
-        
-        # Visualize solution
-        # delete!(vis["xhistLine"])
-        # xhistLine = [Point(x_[1], x_[2], x_[3]) for x_ in x]
-        # setobject!(vis["xhistLine"], Object(PointCloud(xhistLine), 
-        # LineBasicMaterial(color=Colors.RGBA(1,0.6,0.)), "Line"))
 
     end
 
